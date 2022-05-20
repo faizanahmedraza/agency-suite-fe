c948d6e0319e49fd82cc9a83ca7b0f2fb3286cb2
Faizan Ahmed Raza <faizan.raza@saasfa.com>
2022-05-20 11:10:33 +0500

    add changes

--- src/redux/V1/PortalSetting/UPDATE/PortalSettingUpdateReducer.js
+++ src/redux/V1/PortalSetting/UPDATE/PortalSettingUpdateReducer.js
@@ -1,9 +1,11 @@
 import PORTAL_SETTING from "@store/V1/PortalSetting/ActionTypes";
+import SettingHelper from "@src/Helpers/SettingHelper";
 
 const PortalSettingUpdateReducer = (
     state = {
         loading: false,
-        portal_settings: {}
+        portal_settings: localStorage.getItem("portal_settings") ? SettingHelper.localData(localStorage.getItem("portal_settings")) : {},
+        isChanged: false
     },
     action
 ) => {
@@ -12,17 +14,20 @@ const PortalSettingUpdateReducer = (
             return {
                 ...state,
                 loading: true,
+                isChanged: false
             }
         case PORTAL_SETTING.PORTAL_SETTING_UPDATE_SUCCESS:
             return {
                 ...state,
                 loading: false,
                 portal_settings: action.response.portal_settings,
+                isChanged: true
             }
         case PORTAL_SETTING.PORTAL_SETTING_UPDATE_FAILED:
             return {
                 ...state,
                 loading: false,
+                isChanged: false
             }
         default:
             return state
