277b6dd6ac3794fea0bd1d0c578fbc5ada08f50f
Faizan Ahmed Raza <faizan.raza@saasfa.com>
2022-05-31 20:12:58 +0500

    add changes

--- src/@core/components/routes/PublicRoute.js
+++ src/@core/components/routes/PublicRoute.js
@@ -12,7 +12,7 @@ const PublicRoute = ({ children, route }) => {
 
     if (window.location.hostname + window.location.pathname === config.public_url + '/login') return <Navigate to="/launch" replace />
     
-    if (window.location.hostname !== config.public_url && window.location.hostname+'/register' === window.location.hostname+'/register') return <Navigate to="/customers/register" replace />
+    if (window.location.hostname !== config.public_url && window.location.hostname + window.location.pathname === window.location.hostname+'/register') return <Navigate to="/customers/register" replace />
 
     if (window.location.hostname + window.location.pathname !== config.public_url + '/launch' && window.location.pathname === "/launch") return <Navigate to="/login" replace />
 
