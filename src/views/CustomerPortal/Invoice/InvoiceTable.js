747e12bb99e80bc6839043c5f1cee2f037043ca7
Faizan Ahmed Raza <faizan.raza@saasfa.com>
2022-06-07 11:22:55 +0500

    ui changes

--- src/views/CustomerPortal/Invoice/InvoiceTable.js
+++ src/views/CustomerPortal/Invoice/InvoiceTable.js
@@ -50,70 +50,67 @@ const InvoiceTable = (props) => {
         <div>
             {
                 currentItems.length ?
-
-                    <Table bordered responsive>
-                        <thead>
-                            <tr>
-                                <th>INVOICE NUMBER</th>
-                                <th>SERVICE</th>
-                                <th>AMOUNT</th>
-                                <th>IS PAID</th>
-                                <th>CREATED AT</th>
-                                <th>ACTIONS</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {currentItems && currentItems.map((invoice) => {
-                                return (
-                                    <tr key={invoice.id}>
-                                        <td>{invoice?.invoice_number}</td>
-                                        <td>
-                                            <span className='align-middle fw-bold'>{GeneralHelper.PascalCase(invoice?.customer_service_request?.service?.name)}</span>
-                                        </td>
-                                        <td>{'$'+ Number.parseFloat(invoice?.amount).toFixed(2)}</td>
-                                        <td>{invoice?.is_paid ? 'Yes' : 'No'}</td>
-                                        <td>{formatDate(invoice?.created_at)}</td>
-                                        <td className='text-center'>
-                                            <Link to={`/customer-invoices/detail/${invoice.id}`}>
-                                                <Eye className='me-50' size={20} />
-                                            </Link>
-                                        </td>
-                                    </tr>
-                                )
-                            })
-                            }
-                        </tbody>
-                    </Table>
+                    <>
+                        <Table bordered responsive>
+                            <thead>
+                                <tr>
+                                    <th>INVOICE NUMBER</th>
+                                    <th>SERVICE</th>
+                                    <th>AMOUNT</th>
+                                    <th>IS PAID</th>
+                                    <th>CREATED AT</th>
+                                    <th>ACTIONS</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {currentItems && currentItems.map((invoice) => {
+                                    return (
+                                        <tr key={invoice.id}>
+                                            <td>{invoice?.invoice_number}</td>
+                                            <td>
+                                                <span className='align-middle fw-bold'>{GeneralHelper.PascalCase(invoice?.customer_service_request?.service?.name)}</span>
+                                            </td>
+                                            <td>{'$' + Number.parseFloat(invoice?.amount).toFixed(2)}</td>
+                                            <td>{invoice?.is_paid ? 'Yes' : 'No'}</td>
+                                            <td>{formatDate(invoice?.created_at)}</td>
+                                            <td className='text-center'>
+                                                <Link to={`/customer-invoices/detail/${invoice.id}`}>
+                                                    <Eye className='me-50' size={20} />
+                                                </Link>
+                                            </td>
+                                        </tr>
+                                    )
+                                })
+                                }
+                            </tbody>
+                        </Table>
+                        <div className='d-flex justify-content-end pt-1'>
+                            <ReactPaginate
+                                breakLabel="..."
+                                nextLabel="Next >"
+                                onPageChange={handlePageClick}
+                                pageRangeDisplayed={5}
+                                pageCount={pageCount}
+                                previousLabel="< Previous"
+                                renderOnZeroPageCount={null}
+                                breakClassName={'page-item'}
+                                breakLinkClassName={'page-link'}
+                                containerClassName={'pagination'}
+                                pageClassName={'page-item'}
+                                pageLinkClassName={'page-link'}
+                                previousClassName={'page-item'}
+                                previousLinkClassName={'page-link'}
+                                nextClassName={'page-item'}
+                                nextLinkClassName={'page-link'}
+                                activeClassName={'active'}
+                                forcePage={offset}
+                            />
+                        </div>
+                    </>
                     :
                     <div className="text-center">
                         <strong >No Items Found !</strong>
                     </div>
-            }
-            {
-                props.pagination ?
-                    <div className='d-flex justify-content-end pt-1'>
-                        <ReactPaginate
-                            breakLabel="..."
-                            nextLabel="Next >"
-                            onPageChange={handlePageClick}
-                            pageRangeDisplayed={5}
-                            pageCount={pageCount}
-                            previousLabel="< Previous"
-                            renderOnZeroPageCount={null}
-                            breakClassName={'page-item'}
-                            breakLinkClassName={'page-link'}
-                            containerClassName={'pagination'}
-                            pageClassName={'page-item'}
-                            pageLinkClassName={'page-link'}
-                            previousClassName={'page-item'}
-                            previousLinkClassName={'page-link'}
-                            nextClassName={'page-item'}
-                            nextLinkClassName={'page-link'}
-                            activeClassName={'active'}
-                            forcePage={offset}
-                        />
-                    </div>
-                    : null
             }
         </div>
     )
