dcf17fe4aab7833256effd7de81e1c721b34cbd2
Faizan-Katlia <faizan.hussain@saasfa.com>
2022-06-16 16:58:21 +0500

    Footer color

--- src/@core/layouts/components/footer/index.js
+++ src/@core/layouts/components/footer/index.js
@@ -11,7 +11,7 @@ const Footer = () => {
     <p className='clearfix mb-0'>
       <span className='float-md-start d-block d-md-inline-block mt-25'>
         COPYRIGHT © {new Date().getFullYear()}{' '}
-        <a href='' className="ft-ag-clr" color="" target='_blank'>
+        <a href='#' className="ft-ag-clr" color="" target='_blank'>
           {portal_settings && portal_settings?.agency?.name}
         </a>
         <span className='d-none d-sm-inline-block'>, All Rights Reserved</span>
