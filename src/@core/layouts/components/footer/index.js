553e698703995143860b1d634507651397807888
Faizan-Katlia <faizan.hussain@saasfa.com>
2022-06-24 13:30:37 +0500

    footer fixed

--- src/@core/layouts/components/footer/index.js
+++ src/@core/layouts/components/footer/index.js
@@ -11,7 +11,7 @@ const Footer = () => {
     <p className='clearfix mb-0'>
       <span className='float-md-start d-block d-md-inline-block mt-25'>
         COPYRIGHT © {new Date().getFullYear()}{' '} Powered By <span className="ft-ag-clr"> Agency Suite </span>
-        <span className='d-none d-sm-inline-block'>, All rights Reserved</span>
+        {/* <span className='d-none d-sm-inline-block'>, All rights Reserved</span> */}
       </span>
     </p>
   )
